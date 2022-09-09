fa601476e5f4c55db44f676315781e0f5a8ce559
Adegoke Opeoluwa <adegokejohn15@yahoo.com>
2022-09-09 01:37:39 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - print the string in the put function
 *
@@ -12,8 +13,8 @@ int main(void)
 	int i;
 	long li;
 	long long lli;
-	float f;
-	
+	float f
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
